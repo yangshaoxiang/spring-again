e629f3ee4a3a748e297e8be3e7f9e4ba58cb49a7
yangshaoxiang <1330160658@qq.com>
2019-06-27 19:28:02 +0800

    在主类发布事件

--- src/main/java/com/springstudy/springstudy/spring_base/annotationtest/listener/SelfApplicationListener.java
+++ src/main/java/com/springstudy/springstudy/spring_base/annotationtest/listener/SelfApplicationListener.java
@@ -12,17 +12,3 @@ public class SelfApplicationListener implements ApplicationListener {
         System.out.println("SelfApplicationListener 接受到了一个事件"+event);
     }
 }
-
- /*   public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MainConfig.class);
-        //手动发布一个事件
-        ctx.publishEvent(new ApplicationEvent("我手动发布了一个事件") {
-            @Override
-            public Object getSource() {
-                return super.getSource();
-            }
-        });
-
-        //容器关闭也发布事件
-        ctx.close();
-    }*/
