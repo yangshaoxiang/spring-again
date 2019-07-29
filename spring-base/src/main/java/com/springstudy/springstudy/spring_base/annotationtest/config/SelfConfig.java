f70d3da75d58bd1531d00daf49607f8da409e7f7
yangshaoxiang <1330160658@qq.com>
2019-07-29 15:53:54 +0800

    异步多播器配置

--- spring-base/src/main/java/com/springstudy/springstudy/spring_base/annotationtest/config/SelfConfig.java
+++ spring-base/src/main/java/com/springstudy/springstudy/spring_base/annotationtest/config/SelfConfig.java
@@ -40,6 +40,13 @@ public class SelfConfig {
 
     /**
      *  配置支持异步的事件多播器
+     *   配置原因:
+     *   1. 事件多播器的第一次使用是在reflush().initApplicationEventMulticaster()中使用
+     *      根据代码逻辑，若容器中有 SimpleApplicationEventMulticaster 则使用容器中的，否则创建一个默认的非异步的多播器
+     *
+     *   2. 因此我们自己配置一个带有线程池的多播器即可支持异步，问题是多播器的另外一个参数 BeanFactory 如何配置
+     *      答案是不用配置 SimpleApplicationEventMulticaster 实现了 BeanFactoryAware 接口 在容器创建bean时 会自动注入该参数
+     *      在 AbstractAutowireCapableBeanFactory.invokeAwareMethods()中注入
      */
     @Bean
     public SimpleApplicationEventMulticaster applicationEventMulticaster(){
