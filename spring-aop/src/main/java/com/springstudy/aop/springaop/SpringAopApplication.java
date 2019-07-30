1f610b63214b185e1f94f2b868b59d2fd3ed1906
yangshaoxiang <1330160658@qq.com>
2019-07-30 23:45:59 +0800

    aop模块

---
+++ spring-aop/src/main/java/com/springstudy/aop/springaop/SpringAopApplication.java
@@ -0,0 +1,15 @@
+package com.springstudy.aop.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class SpringAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class, args);
+    }
+
+}
